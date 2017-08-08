df6c3b6723c4c8844ef709df56453435d6a51704
Danilo Lopes da Silva <danilo.silva@cjf.jus.br>
2017-08-08 14:53:03 -0300

    Primeiro arquivo de teste

---
+++ Aula01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 01</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<input id="btn" type="button" value="Clique aqui"/>
+
+<script>
+    $("#btn").click(function(){
+        alert('Você clicou no botão.');
+    });
+</script>
+
+</body>
+</html>
