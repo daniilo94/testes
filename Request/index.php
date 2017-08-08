4f72fbc412275f52fc07702b4c7ed2e204f52a30
Danilo Lopes da Silva <danilo.silva@cjf.jus.br>
2017-08-08 15:04:38 -0300

    Teste de envio de pasta com arquivos

---
+++ Request/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danilo.silva
+ * Date: 04/08/2017
+ * Time: 17:18
+ */
+
+require_once 'Request.php';
+
+//$request = new Request();
+//
+//var_dump($request);
+
+//var_dump($_SERVER);
